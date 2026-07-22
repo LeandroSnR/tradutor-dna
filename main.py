990812c934986197aed31981e27512600f67f150
LeandroSnR <leandroabiliosilva@gmail.com>
2026-07-22 19:37:39 -0300

    Iniciando projeto

---
+++ main.py
@@ -0,0 +1,47 @@
+codigo_genetico = {
+    "TTT":"F","TTC":"F","TTA":"L","TTG":"L",
+    "CTT":"L","CTC":"L","CTA":"L","CTG":"L",
+    "ATT":"I","ATC":"I","ATA":"I","ATG":"M",
+    "GTT":"V","GTC":"V","GTA":"V","GTG":"V",
+    "TCT":"S","TCC":"S","TCA":"S","TCG":"S",
+    "CCT":"P","CCC":"P","CCA":"P","CCG":"P",
+    "ACT":"T","ACC":"T","ACA":"T","ACG":"T",
+    "GCT":"A","GCC":"A","GCA":"A","GCG":"A",
+    "TAT":"Y","TAC":"Y","TAA":"*","TAG":"*",
+    "CAT":"H","CAC":"H","CAA":"Q","CAG":"Q",
+    "AAT":"N","AAC":"N","AAA":"K","AAG":"K",
+    "GAT":"D","GAC":"D","GAA":"E","GAG":"E",
+    "TGT":"C","TGC":"C","TGA":"*","TGG":"W",
+    "CGT":"R","CGC":"R","CGA":"R","CGG":"R",
+    "AGT":"S","AGC":"S","AGA":"R","AGG":"R",
+    "GGT":"G","GGC":"G","GGA":"G","GGG":"G"
+}
+
+def traduzir(dna, tabela):
+    dna = dna.upper()
+    proteina = ""
+
+    inicio = dna.find("ATG")
+    if inicio == -1:
+        return "Não existe códon de início."
+    else:
+        dna = dna[inicio:]
+
+    for i in range(0, len(dna)-2, 3):
+        codon = dna[i:i+3]
+
+        aminoacido = tabela.get(codon)
+
+        if aminoacido is None:
+            proteina += "?"
+            continue
+
+        if aminoacido == "*":
+            break
+
+        proteina += aminoacido
+
+    return proteina
+
+sequencia = "ATGGTTTTAAAATAA"
+print(traduzir(sequencia, codigo_genetico))
